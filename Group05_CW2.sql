685c4c548d1df0f3f15bc850c2523fbd717036b7
leomosley <leo.mosley2@gmail.com>
2023-12-28 21:55:19 +0000

    fixed contraints

--- Group05_CW2.sql
+++ Group05_CW2.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+
 CREATE TABLE student (
   student_id SERIAL PRIMARY KEY,
   student_number INT NOT NULL UNIQUE,
@@ -9,15 +11,12 @@ CREATE TABLE student (
   student_addr2 VARCHAR(30),
   student_city VARCHAR(30) NOT NULL,
   student_postcode CHAR(8) NOT NULL,
-  student_edu_email VARCHAR(150) NOT NULL,
-  student_personal_email VARCHAR(150),
+  student_edu_email CITEXT NOT NULL UNIQUE,
+  student_personal_email CITEXT,
   student_landline VARCHAR(30) UNIQUE,
   student_mobile VARCHAR(15) NOT NULL UNIQUE,
-  student_dob DATE NOT NULL,
-
-  CONSTRAINT unique_edu_email UNIQUE (LOWER(student_edu_email)),
-  CONSTRAINT unique_personal_email UNIQUE (LOWER(student_personal_email))
-); 
+  student_dob DATE NOT NULL
+);
 
 CREATE TABLE tuition (
   tuition_id SERIAL PRIMARY KEY,
@@ -26,8 +25,7 @@ CREATE TABLE tuition (
   tuition_remaining DECIMAL(7, 2) NOT NULL,
   tuition_remaining_perc DECIMAL(5, 2) NOT NULL,
   tuition_deadline DATE NOT NULL,
-
-  CONSTRAINT valid_tuition_amount CHECK tuition_amount >= 0,
+  CONSTRAINT valid_tuition_amount CHECK (tuition_amount >= 0),
   CONSTRAINT valid_tuition_paid CHECK (tuition_paid >= 0 AND tuition_paid <= tuition_amount),
   CONSTRAINT valid_tuition_remaining CHECK (tuition_remaining >= 0 AND tuition_remaining <= tuition_amount),
   CONSTRAINT valid_tuition_remaining_perc CHECK (tuition_remaining_perc >= 0 AND tuition_remaining_perc <= 100)
@@ -38,8 +36,7 @@ CREATE TABLE tuition_payment (
   tuition_payment_reference VARCHAR(12) NOT NULL UNIQUE, 
   tuition_payment_amount DECIMAL(7, 2) NOT NULL,
   tuition_payment_date DATE NOT NULL,
-
-  CONSTRAINT valid_tuition_payment_amount CHECK tuition_payment_amount >= 0
+  CONSTRAINT valid_tuition_payment_amount CHECK (tuition_payment_amount >= 0)
 );
 
 CREATE TABLE student_payments (
@@ -76,14 +73,11 @@ CREATE TABLE staff (
   staff_addr2 VARCHAR(30),
   staff_city VARCHAR(30) NOT NULL,
   staff_postcode CHAR(8) NOT NULL,
-  staff_company_email VARCHAR(150) NOT NULL,
-  staff_personal_email VARCHAR(150),
+  staff_company_email CITEXT NOT NULL UNIQUE,
+  staff_personal_email CITEXT UNIQUE,
   staff_landline VARCHAR(30) UNIQUE,
   staff_mobile VARCHAR(15) NOT NULL UNIQUE,
-  staff_dob DATE NOT NULL,
-
-  CONSTRAINT unique_company_email UNIQUE (LOWER(staff_company_email)),
-  CONSTRAINT unique_personal_email UNIQUE (LOWER(staff_personal_email))
+  staff_dob DATE NOT NULL
 );
 
 CREATE TABLE salary (
@@ -92,9 +86,8 @@ CREATE TABLE salary (
   salary_bonuses DECIMAL(8, 2) NOT NULL,
   salary_start_date DATE NOT NULL,
   salary_end_date DATE,
-
-  CONSTRAINT valid_base CHECK salary_base >= 0,
-  CONSTRAINT valid_bonus CHECK salary_bonuses >= 0
+  CONSTRAINT valid_base CHECK (salary_base >= 0),
+  CONSTRAINT valid_bonus CHECK (salary_bonuses >= 0)
 );
 
 CREATE TABLE staff_salary (
@@ -110,8 +103,7 @@ CREATE TABLE hours (
   start_time TIME NOT NULL,
   end_time TIME NOT NULL,
   date DATE NOT NULL,
-
-  CONSTRAINT valid_times CHECK start_time < end_time
+  CONSTRAINT valid_times CHECK (start_time < end_time)
 );
 
 CREATE TABLE staff_hours (
@@ -127,8 +119,7 @@ CREATE TABLE deduction (
   deduction_title VARCHAR(50),
   deduction_details VARCHAR(200),
   deduction_amount DECIMAL(8, 2) NOT NULL,
-
-  CONSTRAINT valid_deduction CHECK deduction_amount >= 0 
+  CONSTRAINT valid_deduction CHECK (deduction_amount >= 0) 
 );
 
 CREATE TABLE salary_payslip (
@@ -144,10 +135,8 @@ CREATE TABLE salary_payslip (
   tax_period INT NOT NULL,
   national_insurance_num CHAR(9) NOT NULL,
   hourly_rate DECIMAL(6, 2) NOT NULL,
-
   CONSTRAINT valid_net_pay CHECK (net_pay >= 0 AND net_pay <= gross_pay),
   CONSTRAINT valid_gross_pay CHECK (gross_pay >= 0 AND gross_pay >= net_pay),
-  
   FOREIGN KEY (salary_id) REFERENCES salary (salary_id)
 );
 
@@ -156,13 +145,12 @@ CREATE TABLE salary_deduction (
   salary_id INT,
   PRIMARY KEY (deduction_id, salary_id),
   FOREIGN KEY (deduction_id) REFERENCES deduction (deduction_id),
-  FOREIGN KEY (salary) REFERENCES salary (salary_id) 
+  FOREIGN KEY (salary_id) REFERENCES salary (salary_id) 
 );
 
 CREATE TABLE department_staff (
   dep_staff_id SERIAL PRIMARY KEY,
   dep_id INT NOT NULL,
-
   FOREIGN KEY (dep_id) REFERENCES departments (dep_id)
 );
 
@@ -170,7 +158,6 @@ CREATE TABLE department_staff_list (
   dep_staff_id INT,
   staff_id INT,
   staff_dep_head BOOLEAN NOT NULL,
-
   PRIMARY KEY (dep_staff_id, staff_id),
   FOREIGN KEY (dep_staff_id) REFERENCES department_staff (dep_staff_id),
   FOREIGN KEY (staff_id) REFERENCES staff (staff_id)
@@ -181,7 +168,6 @@ CREATE TABLE teachers (
   staff_id INT NOT NULL,
   teacher_role VARCHAR(50) NOT NULL,
   teacher_research_area VARCHAR(150),
-
   FOREIGN KEY (staff_id) REFERENCES staff (staff_id)
 );
 
@@ -192,16 +178,13 @@ CREATE TABLE courses (
   course_code INT NOT NULL UNIQUE,
   course_description TEXT,
   course_length SMALLINT NOT NULL,
-
-  CONSTRAINT valid_course_length CHECK course_length > 0,
-
+  CONSTRAINT valid_course_length CHECK (course_length > 0),
   FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id)
 );
 
 CREATE TABLE department_courses (
   dep_id INT,
   course_id INT,
-
   PRIMARY KEY (dep_id, course_id),
   FOREIGN KEY (dep_id) REFERENCES departments (dep_id),
   FOREIGN KEY (course_id) REFERENCES courses (course_id)
@@ -209,12 +192,11 @@ CREATE TABLE department_courses (
 
 CREATE TABLE modules (
   module_id SERIAL PRIMARY KEY,
-  teacher_id NOT NULL,
+  teacher_id INT NOT NULL,
   module_name VARCHAR(50) NOT NULL,
   module_code INT NOT NULL UNIQUE,
   module_description TEXT,
   academ_lvl CHAR(2) NOT NULL,
-
   FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id)
 );
 
@@ -222,8 +204,8 @@ CREATE TABLE course_module (
   course_id INT,
   module_id INT,
   PRIMARY KEY (course_id, module_id),
-  FOREIGN KEY (course_id) REFERENCES courses (course_id)
-  FOREIGN KEY (module_id) REFERENCES modules (module_id),
+  FOREIGN KEY (course_id) REFERENCES courses (course_id),
+  FOREIGN KEY (module_id) REFERENCES modules (module_id)
 );
 
 CREATE TABLE student_course (
@@ -246,9 +228,7 @@ CREATE TABLE student_course_progress (
   student_id INT,
   course_id INT,
   progress_perc DECIMAL(5, 2) NOT NULL,
-
   CONSTRAINT valid_progress_perc CHECK (progress_perc >= 0 AND progress_perc <= 100),
-
   PRIMARY KEY (student_id, course_id),
   FOREIGN KEY (student_id) REFERENCES student (student_id),
   FOREIGN KEY (course_id) REFERENCES courses (course_id)
@@ -258,9 +238,7 @@ CREATE TABLE student_module_progress (
   student_id INT,
   module_id INT,
   progress_perc DECIMAL(5, 2) NOT NULL,
-
   CONSTRAINT valid_progress_perc CHECK (progress_perc >= 0 AND progress_perc <= 100),
-
   PRIMARY KEY (student_id, module_id),
   FOREIGN KEY (student_id) REFERENCES student (student_id),
   FOREIGN KEY (module_id) REFERENCES modules (module_id)
@@ -276,9 +254,7 @@ CREATE TABLE module_assignments (
   assignment_due_time TIME NOT NULL,
   assignment_description TEXT,
   assignment_type VARCHAR(100) NOT NULL,
-  
-  CONSTRAINT valid_due_date CHECK assignment_due_date >= assignment_set_date,
-  
+  CONSTRAINT valid_due_date CHECK (assignment_due_date >= assignment_set_date),
   FOREIGN KEY (module_id) REFERENCES modules (module_id)
 );
 
@@ -293,10 +269,8 @@ CREATE TABLE module_assessments (
   assessment_description TEXT,
   assessment_type VARCHAR(100) NOT NULL,
   assessment_weighting DECIMAL(5, 2) NOT NULL,
-
-  CONSTRAINT valid_due_date CHECK assessment_due_date >= assessment_set_date,
+  CONSTRAINT valid_due_date CHECK (assessment_due_date >= assessment_set_date),
   CONSTRAINT valid_weighting CHECK (assessment_weighting >= 0 AND assessment_weighting <= 100),
-
   FOREIGN KEY (module_id) REFERENCES modules (module_id)
 );
 
@@ -304,9 +278,7 @@ CREATE TABLE module_assessment_grade (
   assessment_id INT,
   student_id INT,
   assessment_grade DECIMAL(5, 2) NOT NULL,
-
-  CONSTRAINT valid_grade CHECK assessment_grade >= 0,
-
+  CONSTRAINT valid_grade CHECK (assessment_grade >= 0),
   PRIMARY KEY (assessment_id, student_id),
   FOREIGN KEY (assessment_id) REFERENCES module_assessments (assessment_id),
   FOREIGN KEY (student_id) REFERENCES student (student_id)
@@ -316,9 +288,7 @@ CREATE TABLE module_assignment_grade (
   assignment_id INT,
   student_id INT,
   assignment_grade DECIMAL(5, 2) NOT NULL,
-
-  CONSTRAINT valid_grade CHECK assignment_grade >= 0,
-
+  CONSTRAINT valid_grade CHECK (assignment_grade >= 0),
   PRIMARY KEY (assignment_id, student_id),
   FOREIGN KEY (assignment_id) REFERENCES module_assignments (assignment_id),
   FOREIGN KEY (student_id) REFERENCES student (student_id)
@@ -329,12 +299,10 @@ CREATE TABLE teaching_session (
   module_id INT NOT NULL,
   session_type VARCHAR(50) NOT NULL,
   session_start_time TIME NOT NULL,
-  session_length INTERVAL NOT NULL,
+  session_length DECIMAL(6,2) NOT NULL,
   session_date DATE NOT NULL,
   session_notes TEXT,
-
-  CONSTRAINT valid_length CHECK session_length > 0,
-
+  CONSTRAINT valid_length CHECK (session_length > 0),
   FOREIGN KEY (module_id) REFERENCES modules (module_id)
 );
 
@@ -351,19 +319,17 @@ CREATE TABLE academic_help_sessions (
   student_id INT NOT NULL,
   ah_session_type VARCHAR(50) NOT NULL,
   ah_session_start_time TIME NOT NULL,
-  ah_session_length INTERVAL NOT NULL,
+  ah_session_length DECIMAL(6,2) NOT NULL,
   ah_session_date DATE NOT NULL,
   ah_session_notes TEXT,
-
-  CONSTRAINT valid_length CHECK ah_session_length > 0,
-
+  CONSTRAINT valid_length CHECK (ah_session_length > 0),
   FOREIGN KEY (student_id) REFERENCES student (student_id)
 );
 
 CREATE TABLE teachers_ah_sessions (
   teacher_id INT,
   ah_session_id INT,
-  PRIMARY KEY (teacher_id, session_id),
+  PRIMARY KEY (teacher_id, ah_session_id),
   FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id),
   FOREIGN KEY (ah_session_id) REFERENCES academic_help_sessions (ah_session_id)
 );
@@ -372,7 +338,6 @@ CREATE TABLE attendance (
   student_id INT,
   session_id INT,
   attendance_record BOOLEAN NOT NULL,
-
   PRIMARY KEY (student_id, session_id),
   FOREIGN KEY (student_id) REFERENCES student (student_id),
   FOREIGN KEY (session_id) REFERENCES teaching_session (session_id)
