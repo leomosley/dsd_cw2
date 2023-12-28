077747a01fdf1f48c0259383f0e957a96f9e92f1
leomosley <leo.mosley2@gmail.com>
2023-12-28 20:53:19 +0000

    added all current tables

--- Group05_CW2.sql
+++ Group05_CW2.sql
@@ -59,6 +59,134 @@ CREATE TABLE student_tuition (
   FOREIGN KEY (tuition_id) REFERENCES tuition (tuition_id)
 );
 
+CREATE TABLE departments (
+  dep_id SERIAL PRIMARY KEY,
+  dep_name VARCHAR(50) NOT NULL,
+  dep_type VARCHAR(50) NOT NULL,
+  dep_description VARCHAR(200)
+);
+
+CREATE TABLE staff (
+  staff_id SERIAL PRIMARY KEY,
+  staff_number INT NOT NULL UNIQUE,
+  staff_fname VARCHAR(50) NOT NULL,
+  staff_mname VARCHAR(50),
+  staff_lname VARCHAR(50) NOT NULL,
+  staff_pronouns VARCHAR(50) NOT NULL,
+  staff_addr1 VARCHAR(30) NOT NULL,
+  staff_addr2 VARCHAR(30),
+  staff_city VARCHAR(30) NOT NULL,
+  staff_postcode CHAR(8) NOT NULL,
+  staff_company_email VARCHAR(150) NOT NULL,
+  staff_personal_email VARCHAR(150),
+  staff_landline VARCHAR(30) UNIQUE,
+  staff_mobile VARCHAR(15) NOT NULL UNIQUE,
+  staff_dob DATE NOT NULL,
+
+  -- Check emails are actually unique
+  CONSTRAINT unique_company_email UNIQUE (LOWER(staff_company_email)),
+  CONSTRAINT unique_personal_email UNIQUE (LOWER(staff_personal_email))
+);
+
+CREATE TABLE salary (
+  salary_id SERIAL PRIMARY KEY,
+  salary_base DECIMAL(8, 2) NOT NULL,
+  salary_bonuses DECIMAL(8, 2) NOT NULL,
+  salary_start_date DATE NOT NULL,
+  salary_end_date DATE,
+
+  CONSTRAINT valid_base CHECK salary_base >= 0,
+  CONSTRAINT valid_bonus CHECK salary_bonuses >= 0
+);
+
+CREATE TABLE staff_salary (
+  salary_id INT,
+  staff_id INT,
+  PRIMARY KEY (salary_id, staff_id),
+  FOREIGN KEY (salary_id) REFERENCES salary (salary_id),
+  FOREIGN KEY (staff_id) REFERENCES staff (staff_id)
+);
+
+CREATE TABLE hours (
+  hour_id SERIAL PRIMARY KEY,
+  start_time TIME NOT NULL,
+  end_time TIME NOT NULL,
+  date DATE NOT NULL,
+
+  CONSTRAINT valid_times CHECK start_time < end_time
+);
+
+CREATE TABLE staff_hours (
+  hour_id INT,
+  staff_id INT,
+  PRIMARY KEY (hour_id, staff_id),
+  FOREIGN KEY (hour_id) REFERENCES salary (hour_id),
+  FOREIGN KEY (staff_id) REFERENCES staff (staff_id) 
+);
+
+CREATE TABLE deduction (
+  deduction_id SERIAL PRIMARY KEY,
+  deduction_title VARCHAR(50),
+  deduction_details VARCHAR(200),
+  deduction_amount DECIMAL(8, 2) NOT NULL,
+
+  CONSTRAINT valid_deduction REFERENCES deduction_amount >= 0 
+);
+
+CREATE TABLE salary_payslip (
+  payslip_id SERIAL PRIMARY KEY,
+  salary_id INT NOT NULL,
+  issue_date DATE NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  net_pay DECIMAL(8, 2) NOT NULL,
+  gross_pay DECIMAL(8, 2) NOT NULL,
+  payment_method VARCHAR(30) NOT NULL,
+  tax_code VARCHAR(10) NOT NULL,
+  tax_period INT NOT NULL,
+  national_insurance_num CHAR(9) NOT NULL,
+  hourly_rate DECIMAL(6, 2) NOT NULL,
+
+  CONSTRAINT valid_net_pay CHECK (net_pay >= 0 AND net_pay <= gross_pay),
+  CONSTRAINT valid_gross_pay CHECK (gross_pay >= 0 AND gross_pay >= gross_pay),
+  
+  FOREIGN KEY (salary_id) REFERENCES salary (salary_id)
+);
+
+CREATE TABLE salary_deduction (
+  deduction_id INT,
+  salary_id INT,
+  PRIMARY KEY (deduction_id, salary_id),
+  FOREIGN KEY (deduction_id) REFERENCES deduction (deduction_id),
+  FOREIGN KEY (salary) REFERENCES salary (salary_id) 
+);
+
+CREATE TABLE department_staff (
+  dep_staff_id SERIAL PRIMARY KEY,
+  dep_id INT NOT NULL,
+
+  FOREIGN KEY (dep_id) REFERENCES departments (dep_id)
+);
+
+CREATE TABLE department_staff_list (
+  dep_staff_id INT,
+  staff_id INT,
+  staff_dep_head BOOLEAN NOT NULL,
+
+  PRIMARY KEY (dep_staff_id, staff_id),
+  FOREIGN KEY (dep_staff_id) REFERENCES department_staff (dep_staff_id),
+  FOREIGN KEY (staff_id) REFERENCES staff (staff_id)
+);
+
+CREATE TABLE teachers (
+  teacher_id SERIAL PRIMARY KEY,
+  staff_id INT NOT NULL,
+  teacher_role VARCHAR(50) NOT NULL,
+  teacher_research_area VARCHAR(150),
+
+  FOREIGN KEY (staff_id) REFERENCES staff (staff_id)
+);
+
 CREATE TABLE courses (
   course_id SERIAL PRIMARY KEY,
   teacher_id INT NOT NULL,
@@ -70,6 +198,15 @@ CREATE TABLE courses (
   CONSTRAINT valid_course_length CHECK course_length > 0,
 
   FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id)
+);
+
+CREATE TABLE department_courses (
+  dep_id INT,
+  course_id INT,
+
+  PRIMARY KEY (dep_id, course_id),
+  FOREIGN KEY (dep_id) REFERENCES departments (dep_id),
+  FOREIGN KEY (course_id) REFERENCES courses (course_id)
 );
 
 CREATE TABLE modules (
@@ -142,10 +279,103 @@ CREATE TABLE module_assignments (
   assignment_description TEXT,
   assignment_type VARCHAR(100) NOT NULL,
   
-  CONSTRAINT valid_due_date CONSTRAINT assignment_due_date >= assignment_set_date,
+  CONSTRAINT valid_due_date CHECK assignment_due_date >= assignment_set_date,
+  
+  FOREIGN KEY (module_id) REFERENCES modules (module_id)
+);
+
+CREATE TABLE module_assessments (
+  assessment_id SERIAL PRIMARY KEY,
+  module_id INT NOT NULL,
+  assessment_title VARCHAR(50) NOT NULL,
+  assessment_set_date DATE NOT NULL,
+  assessment_due_date DATE NOT NULL,
+  assessment_set_time TIME NOT NULL,
+  assessment_due_time TIME NOT NULL,
+  assessment_description TEXT,
+  assessment_type VARCHAR(100) NOT NULL,
+  assessment_weighting DECIMAL(5, 2) NOT NULL,
+
+  CONSTRAINT valid_due_date CHECK assessment_due_date >= assessment_set_date,
+  CONSTRAINT valid_weighting CHECK (assessment_weighting >= 0 AND assessment_weighting <= 100),
+
   FOREIGN KEY (module_id) REFERENCES modules (module_id)
 );
 
 CREATE TABLE module_assessment_grade (
-  
+  assessment_id INT,
+  student_id INT,
+  assessment_grade DECIMAL(5, 2) NOT NULL,
+
+  CONSTRAINT valid_grade CHECK assessment_grade >= 0,
+
+  PRIMARY KEY (assessment_id, student_id),
+  FOREIGN KEY (assessment_id) REFERENCES module_assessments (assessment_id),
+  FOREIGN KEY (student_id) REFERENCES student (student_id)
+);
+
+CREATE TABLE module_assignment_grade (
+  assignment_id INT,
+  student_id INT,
+  assignment_grade DECIMAL(5, 2) NOT NULL,
+
+  CONSTRAINT valid_grade CHECK assignment_grade >= 0,
+
+  PRIMARY KEY (assignment_id, student_id),
+  FOREIGN KEY (assignment_id) REFERENCES module_assignments (assignment_id),
+  FOREIGN KEY (student_id) REFERENCES student (student_id)
+);
+
+CREATE TABLE teaching_session (
+  session_id SERIAL PRIMARY KEY,
+  module_id INT NOT NULL,
+  session_type VARCHAR(50) NOT NULL,
+  session_start_time TIME NOT NULL,
+  session_length INTERVAL NOT NULL,
+  session_date DATE NOT NULL,
+  session_notes TEXT,
+
+  CONSTRAINT valid_length CHECK session_length > 0,
+
+  FOREIGN KEY (module_id) REFERENCES modules (module_id)
+);
+
+CREATE TABLE teachers_sessions (
+  teacher_id INT,
+  session_id INT,
+  PRIMARY KEY (teacher_id, session_id),
+  FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id),
+  FOREIGN KEY (session_id) REFERENCES teaching_session (session_id)
+);
+
+CREATE TABLE academic_help_sessions (
+  ah_session_id SERIAL PRIMARY KEY,
+  student_id INT NOT NULL,
+  ah_session_type VARCHAR(50) NOT NULL,
+  ah_session_start_time TIME NOT NULL,
+  ah_session_length INTERVAL NOT NULL,
+  ah_session_date DATE NOT NULL,
+  ah_session_notes TEXT,
+
+  CONSTRAINT valid_length CHECK ah_session_length > 0,
+
+  FOREIGN KEY (student_id) REFERENCES student (student_id)
+);
+
+CREATE TABLE teachers_ah_sessions (
+  teacher_id INT,
+  ah_session_id INT,
+  PRIMARY KEY (teacher_id, session_id),
+  FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id),
+  FOREIGN KEY (ah_session_id) REFERENCES academic_help_sessions (ah_session_id)
+);
+
+CREATE TABLE attendance (
+  student_id INT,
+  session_id INT,
+  attendance_record BOOLEAN NOT NULL,
+
+  PRIMARY KEY (student_id, session_id),
+  FOREIGN KEY (student_id) REFERENCES student (student_id),
+  FOREIGN KEY (session_id) REFERENCES teaching_session (session_id)
 );
